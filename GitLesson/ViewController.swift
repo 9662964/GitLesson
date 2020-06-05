e42f157b0b61a1e4f4d08fce19adce7af2478478
iljoochae <9662964@gmail.com>
2020-06-05 17:07:20 -0400

    Initial Commit

---
+++ GitLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson
+//
+//  Created by iljoo Chae on 6/5/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
